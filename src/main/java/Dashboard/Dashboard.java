b95b58d91de7044dd5e0e31b103a407b19f2e082
Thien Hoang <thienvhoang99@gmail.com>
2020-10-20 01:15:00 +0700

    Restructure

--- src/main/java/Dashboard/Dashboard.java
+++ src/main/java/Dashboard/Dashboard.java
@@ -1,4 +1,4 @@
-package com.company;
+package Dashboard;
 import javax.print.attribute.standard.JobMediaSheetsCompleted;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
